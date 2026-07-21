a2a8b0e298daa56c87252abb0001ca25e36f2f4f
Alex Corona <alex_corona80@outlook.com>
2026-07-20 20:15:59 -0600

    Se crea proyecto en Spring con dependencias basicas para el proyecto

---
+++ API/API/src/main/java/com/team70/API/dto/ContentResponse.java
@@ -0,0 +1,21 @@
+package com.team70.API.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContentResponse {
+
+    private String categoria;
+    private Double probabilidad;
+    private List<String> informacionAdicional;
+    private String modeloUtilizado;
+    private Long tiempoProcesamientoMs;
+}
